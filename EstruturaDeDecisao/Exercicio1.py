0cae311edc178121d80a15110e16334e31f7c4f4
claudiopmaia <claudiopmaia@gmail.com>
2019-01-15 15:55:23 -0300

    add Exercicio 2

--- EstruturaDeDecisao/Exercicio1.py
+++ EstruturaDeDecisao/Exercicio1.py
@@ -1,1 +1,10 @@
-A, B int(input("Digite dois numeros"))
+#Faça um Programa que peça dois números e imprima o maior deles.
+
+A, B = input("Digite dois numeros ").split()
+A = int(A)
+B = int(B)
+
+if A > B:
+    print("O numero maior dos dois foi o primeiro numero que é:", A)
+else:
+    print("O numero maior dos dois foi segundo numero que foi o:", B)
---
+++ EstruturaDeDecisao/Exercicio2.py
@@ -0,0 +1,9 @@
+#Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+A = int(input("Digite um numeros "))
+
+
+if A > 0:
+    print("O numero é positivo: ")
+else:
+    print("O numero é negativo: ")
